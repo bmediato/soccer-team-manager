5f8992a850a6e09e965343e8a3b7cc46884ccc55
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-02 13:56:26 -0300

    fix: retira hello world

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
 app.post('/teams', (req, res) => {
